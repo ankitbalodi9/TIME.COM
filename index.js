b45cabad1af0e2818ac66f3a3c9556bef2e7c7b5
ankitbalodi9 <ankitbalodi14@gmail.com>
2019-04-17 00:13:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
+var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
-var url = 'http://time.com/';
-request(url, function (error, response, html) {
-  const $ = cheerio.load(html);
+
+url = 'http://time.com/';
+request(url, function(error, response, html){
+  if(!error){
+    var $ = cheerio.load(html);
+    var json = {
+      news : {}      
+      };
+    
     $('.last-column .column-tout .column-tout-info .column-tout-metadata a').each((i,el) =>{
-      const nam = $(el).text().replace(/\s\s+/g, '');
-      const link = $(el).attr('href')
-      console.log(nam,"\n");
-      console.log('http://time.com'+link,"\n");                                                                              
-    });                                                                
+      const Title = $(el).text().replace(/\s\s+/g, '');
+      const link = $(el).attr('href');
+      var title = "title: "+ Title;
+      var lenk = "link: http://time.com"+ link;
+      json.news[i] = [title,lenk];
+     });
+  }
+  fs.writeFile('output.json', JSON.stringify(json, null,1), function(err){
+    console.log("done");
+  });
 });
